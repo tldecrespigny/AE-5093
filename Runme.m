079bf999611987b26905f7b7ebe1a4b64ebcc2ca
tldecrespigny <42721078+tldecrespigny@users.noreply.github.com>
2021-02-11 19:28:59 -0500

    Beeg update

--- Runme.m
+++ Runme.m
@@ -1,85 +1,96 @@
 clc; clear all; close all;
-
-%Initial Conditions
-mp = 200; %propellant mass kg
-mpl = 300; 
-m0 = mp + mpl;
+global  g R a1 expo Tsl T11 rho0 Vflow Pexit Aexit Me Te m0 mp m_dot;
+%% Initial Conditions
+g = 9.80665;    % m/s^2
+R = 287;        % J/(kgK)
+a1 = -.0065;    % K/m
+expo = -g/(a1*R);  % n/a
+Tsl = 288.16;   % K
+T11 = 216.66;   % K
+rho0 = 1.225;   % kg/m^3
+Hmax = 100000; %desired final height of 100km
+deltaV = sqrt(2*g*Hmax);
+psl = 101320; %pa
+a = -.00065; %k/m
+tStep = 1;
+tend = 100;
 tspan=0:tStep:tend;
-tStep = .01;
-tend = 100000;
+tbStep = 1;
+tbend = 25;
 gamma = 1.4;
-g = 9.81;
-r = 287;
 alt = zeros(1,tbend/tbStep);
-t_bo = 30;
 V0 = 0;
-
+%% Bread
+Me = 4;
+Aexit = (.375^2)* pi; %meters
+T0 = 3800; %kelvin
+gamma = 1.4;
+Pexit = 101500; %101.5 kpa
+Mpay = 300;
+[mach, ToverT0, PoverP0, RhooverRho0, AoverAstar] = flowisentropic(gamma, Me);
+combustion_stag_pressure = Pexit/PoverP0;
+Te = ToverT0*T0;
+m0=Mpay/0.05; %from a payload mass fraction of 5%
+Ve = Me*sqrt(gamma*R*Te); 
+Vexit = Me*sqrt(gamma*R*Te);
+MpoverM0 = 1-exp(-deltaV/Vexit);
+mp = MpoverM0*m0;
+Mstructural = m0-mp-Mpay;
+Mf = Mstructural+Mpay;
+At = Aexit/AoverAstar;
+m_dot = (Me*sqrt(gamma*R*Te))*Aexit;
 Pexit = 101500; %101.5 kpa sea level
+m0 = Mstructural+Mpay;
+initial_conditions = [V0,0,m_dot];
+Vflow = Me*sqrt(1.4*R*Te);
+%% Butter
+[t1,r1] = ode45(@f_x,tspan,initial_conditions);
+plot(t1,r1)
+legend(["V_dot" "x_dot" "mdot"])
 
-initial_conditions = [V0,m0,0]
-%Loop
-for tb = 1:tbStep:tbend
-   options=odeset('Events',@bevents);
-   [t1,r1] = ode45(@f_x,tspan,initial_conditions,options);
-    
+%% ODE
+
+function x_t = f_x(t,f)
+global g Vflow Aexit Pexit m0 m_dot mp;
+    V = f(1);
+    rho = dens(f(2));
+    dm = f(3);
+    C_d = .3;
+    mp = mp - dm;
+    m = mp+m0;
+    T = m_dot*Vflow + (Pexit-press(f(2)))*Aexit;
+    V_dot = -g-(.5*rho*C_d*Aexit*(V^2))/m + (T/m);
+    if mp <= 0
+        dm = 0;
+        m_dot = 0;
+        mp = 0;
+        Pexit = 0;
+    end
+    x_dot = V;
+    x_t = [V_dot; x_dot; m_dot;];
 end
 
-
-function rho = rho(h)
+%% Atmospheric Parameters
+function T = temp(h)    % K
+global a1 Tsl 
+    T = Tsl-a1.*h;
+end
+function P = press(h)   % Pa
+global g R expo Tsl T11
+    if h <= 11000
+        P = 101320.*(temp(h)./Tsl).^expo;
+    else
+        P = 22346.*exp(-(g.*(h-11000))./(R.*T11));
+    end
+end
+function rho = dens(h)  % kg/m^3
+global g R a1
     Ts = 288.16;
     Rhos = 1.225;   
-    g = 9.81;
-    a1 = -6.5*(10^-3);
-    R = 287;
     temp = Ts + a1*h;
     rho = Rhos*((temp/Ts)^(-1-(g/(a1*R))));
 end
 
-%ODE
-function x_t = f_x(t,f)
-dv = f(1);
-dm = f(2);
-y = 1.4;
-rhoh = rho(f(3)); 
-mp = mp + dm;
-m = mp+mpl;
-V = V + dv;
-
-V_dot = -g-(.5*rhoh*C_d*A*(V^2))/m + (T/m);
-if t >= t_bo
-    m_dot = 0;
-else
-    m_dot = mdot;
-end
-x_dot = V;
-x_t = [V_dot,m_dot,x_dot];
-end
-
-function m_dot = m_dot(mp1,r,gamma)
-Mexit = 2;
-Aexit = 0.3; %meters
-T0 = 3800; %kelvin
-%m0=mp1/0.05; %from a payload mass fraction of 5%
-gamma = 1.4;
-g = 9.81;
-r = 287;
-Hmax = 100000; %desired final height of 100km
-Pexit = 101500; %101.5 kpa
-Mpay = 300;
-
-[mach, ToverT0, PoverP0, RhooverRho0, AoverAstar] = flowisentropic(1.4, Mexit);
-combustion_stag_pressure = Pexit/PoverP0;
-Texit = ToverT0*T0;
-m0=Mpay/0.05; %from a payload mass fraction of 5%
-Ve = Mexit*sqrt(gamma*r*Texit);
-deltaV = sqrt(2*g*Hmax); %initial estimate
-Vexit = Mexit*sqrt(gamma*r*Texit);
-MpoverM0 = 1-exp(-deltaV/Vexit);
-Mp = MpoverM0*m0;
-Mstructural = m0-Mp-Mpay;
-Mf = Mstructural+Mpay;
-At = Aexit/AoverAstar;
-end
 
 %part inout Me Ae To Hdesign (sea level) 
 
