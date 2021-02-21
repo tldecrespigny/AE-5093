8dc781c7c17190b232d6ebb8cf03c0f48e6a1bd8
BigBird117 <62759054+BigBird117@users.noreply.github.com>
2021-02-21 15:42:12 -0500

    should have seen the other guy

---
+++ Project2.m
@@ -0,0 +1,16 @@
+clc; clear all; close all;
+M = 2.5;
+alt = 5000; %meters
+p = 54000; %54kpa
+t = 225; %k
+gamma = 1.4;
+
+theta2 = 12*(pi/180); %input degrees
+theta3 = -3*(pi/180; %inputdegrees
+beta = ;
+
+function angle = collar(M,theta,gamma)
+x=cot(beta);
+A = M1^2-1;
+B = ((gamma+1)/2)*M1^4*tan(theta)
+end
