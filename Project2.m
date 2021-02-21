da202d9e9ed93e50e50c53cb42d12da85d65162d
tldecrespigny <42721078+tldecrespigny@users.noreply.github.com>
2021-02-21 16:28:13 -0500

    Collar Function Almost Done

--- Project2.m
+++ Project2.m
@@ -1,24 +1,29 @@
 clc; clear all; close all;
-M1 = 2.5;
+global y
+M1 = 2;
 alt = 5000; %meters
-p = 54000; %54kpa
-t = 225; %k
-gamma = 1.4;
+p_s = 54000; %54kpa
+t_s = 225; %k
+y = 1.3;
 
-theta2 = 12*(pi/180); %input degrees
+theta2 = 9*(pi/180); %input degrees
 theta3 = -3*(pi/180); %inputdegrees
 
-beta1 = collar(M1,theta2, gamma)*(180/pi)
+beta1 = collar(M1,theta2)*(180/pi)
 
-function shock_angle = collar(M,theta,gamma)
+
+
+function Beta = collar(M,theta)
+global y
 A = M^2-1;
-B = ((gamma+1)/2)*M^4*tan(theta);
-C = (1+((gamma+1)/2)*M^2)*tan(theta);
+B = ((y+1)/2)*M^4*tan(theta);
+C = (1+((y+1)/2)*M^2)*tan(theta);
+x = zeros(1,20);
 x(1) = sqrt(M^2-1);
 n=2;
-while x(n-1)~= x(n)
-x = sqrt(A-(B/(x+C)));
-shock_angle = acot(x);
-n=n+1;
-end
+    while x(n-1)~= x(n)
+        x(n)= sqrt(A-(B/(x(n-1)+C)));
+        n=n+1;
+    end
+Beta = acot(x);
 end
