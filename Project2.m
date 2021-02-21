f14b7bf5319ef306c28fbe8a9a165a072f29c1d8
BigBird117 <62759054+BigBird117@users.noreply.github.com>
2021-02-21 15:51:12 -0500

    fixed

--- Project2.m
+++ Project2.m
@@ -1,5 +1,5 @@
 clc; clear all; close all;
-M = 2.5;
+M1 = 2.5;
 alt = 5000; %meters
 p = 54000; %54kpa
 t = 225; %k
@@ -8,12 +8,13 @@ gamma = 1.4;
 theta2 = 12*(pi/180); %input degrees
 theta3 = -3*(pi/180); %inputdegrees
 
+beta1 = collar(M1,theta2, gamma)
 
-function angle = collar(M,theta,gamma)
+function shock_angle = collar(M,theta,gamma)
 A = M^2-1;
 B = ((gamma+1)/2)*M^4*tan(theta);
 C = (1+((gamma+1)/2)*M^2)*tan(theta);
 x1 = sqrt(M^2-1);
 x_nplus1 = sqrt(A-(B/(x1+C)));
-beta = acot(x)
+shock_angle = acot(x_nplus1)
 end
