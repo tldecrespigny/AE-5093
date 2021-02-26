7ecf6f3413843790457d94d979d2e12b3368c21c
BigBird117 <62759054+BigBird117@users.noreply.github.com>
2021-02-25 23:51:15 -0500

    ITS DONE

--- Project2.m
+++ Project2.m
@@ -21,11 +21,12 @@ error=100*(P4_P1design-P4_P1off)/(P4_P1design) %under 0.5% error
 
 
 %% Trade Study
-theta2 = 4;
+
 P4_P1ts = zeros(1,10);
 diff_effts = zeros(1,10);
 deltaV4_V4ts = zeros(1,10);
 i = 1;
+theta2=4;
 while theta2 <= 16
   theta3 = -4;
   while theta3 >= -16
@@ -33,10 +34,56 @@ while theta2 <= 16
       theta3 = theta3 -4;
       i = i+1;
   end
-  theta2 = theta2 + 4;
+  theta2= theta2 + 4;
 end
+theta2p=[4:4:16];
+theta3p=[-4:-4:-16];
+difftheta4=[diff_effts(1),diff_effts(2),diff_effts(3),diff_effts(4)];
+difftheta8=[diff_effts(5),diff_effts(6),diff_effts(7),diff_effts(8)]; 
+difftheta12=[diff_effts(9),diff_effts(10),diff_effts(11),diff_effts(12)];
+difftheta16=[diff_effts(13),diff_effts(14),diff_effts(15),diff_effts(16)];
 
-%% Function
+P4_P1theta4=[P4_P1ts(1),P4_P1ts(2),P4_P1ts(3),P4_P1ts(4)];
+P4_P1theta8=[P4_P1ts(5),P4_P1ts(6),P4_P1ts(7),P4_P1ts(8)]; 
+P4_P1theta12=[P4_P1ts(9),P4_P1ts(10),P4_P1ts(11),P4_P1ts(12)];
+P4_P1theta16=[P4_P1ts(13),P4_P1ts(14),P4_P1ts(15),P4_P1ts(16)];
+
+deltaVtheta4=[deltaV4_V4ts(1),deltaV4_V4ts(2),deltaV4_V4ts(3),deltaV4_V4ts(4)];
+deltaVtheta8=[deltaV4_V4ts(5),deltaV4_V4ts(6),deltaV4_V4ts(7),deltaV4_V4ts(8)]; 
+deltaVtheta12=[deltaV4_V4ts(9),deltaV4_V4ts(10),deltaV4_V4ts(11),deltaV4_V4ts(12)];
+deltaVtheta16=[deltaV4_V4ts(13),deltaV4_V4ts(14),deltaV4_V4ts(15),deltaV4_V4ts(16)];
+
+figure(1)
+hold on;
+plot(theta3p,difftheta4);
+plot(theta3p,difftheta8);
+plot(theta3p,difftheta12);
+plot(theta3p,difftheta16);
+xlabel('theta3 (degrees)');
+ylabel('diffuser efficiency');
+legend('theta2 = 4','theta2 = 8','theta2 = 12','theta2 = 16');
+
+figure(2)
+hold on;
+plot(theta3p,P4_P1theta4);
+plot(theta3p,P4_P1theta8);
+plot(theta3p,P4_P1theta12);
+plot(theta3p,P4_P1theta16);
+xlabel('theta3 (degrees)');
+ylabel('P_4/P_1');
+legend('theta2 = 4','theta2 = 8','theta2 = 12','theta2 = 16');
+
+figure(3)
+hold on;
+plot(theta3p,deltaVtheta4);
+plot(theta3p,deltaVtheta8);
+plot(theta3p,deltaVtheta12);
+plot(theta3p,deltaVtheta16);
+xlabel('theta3 (degrees)');
+ylabel('deltaV');
+legend('theta2 = 4','theta2 = 8','theta2 = 12','theta2 = 16');
+
+%% Thee Function
 function [P4_P1, diff_eff, deltaV4_V4prime] = diffuser(theta_2,theta_3, M1,alt, P1,T1)
 global y
 y = 1.3;
